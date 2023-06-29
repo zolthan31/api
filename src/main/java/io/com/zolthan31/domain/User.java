312ba1e0ab20a6a69b8e847b96cf3c616133a7a0
zolthan31 <dhb.tecnologia@outlook.com>
2023-06-29 16:11:55 -0300

    Classe usuario

---
+++ src/main/java/io/com/zolthan31/domain/User.java
@@ -0,0 +1,29 @@
+package io.com.zolthan31.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+	
+	
+
+}
